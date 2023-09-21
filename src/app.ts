87807c4e3f129337ee5ea5d9e81708d006e61e55
flefur <f.lefur@djust.io>
2023-09-21 12:06:08 +0200

    Fix cors error

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express, { Application, Request, Response } from 'express'
 import flightRouter from './routes/flights.routes';
+import cors from 'cors';
 
 const app: Application = express()
 
 const port: number = 8081
+
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+  }));
 
 app.use(flightRouter);
 

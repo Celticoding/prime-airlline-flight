ca6a21a2026ddb2338703b8a014c2c6fe2464196
rathesh77 <rathesh.charles@outlook.fr>
2023-09-22 15:12:11 +0200

    fix discount

--- src/services/discount.service.ts
+++ src/services/discount.service.ts
@@ -1,6 +1,7 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import FlightDto from '../dtos/flight.dto';
 import DiscountDto from '../dtos/discount.dto';
+import FlightService from './flight.service';
 
 
 class DiscountService {
@@ -14,10 +15,16 @@ class DiscountService {
       const discounts = await Promise.all(discountsFromDb.filter((discount: DiscountDto) => discount.originalFlightId === flight.id)
         .map(async(discount: DiscountDto) => {
             delete discount.originalFlightId
-            const discountedFlight = await axios.get(DATA_ACCES_API + "/flight/"+discount.flightId)
+            let discountedFlightResponse: AxiosResponse<any, any> = await axios.get(DATA_ACCES_API + "/flight/"+discount.flightId)
+            const discountedFlight: FlightDto = discountedFlightResponse.data
             delete discount.flightId
             const discountPrice = +(price - (price * (discount.percent / 100))).toFixed(2);
-            return { ...discount, discountPrice, flight: discountedFlight.data };
+            discountedFlight.date =  flight.date!;
+            discountedFlight.seats =  await FlightService.getAvailableSeats(discountedFlight.id, flight.date!);
+
+            let updatedFlight = {...flight}
+            updatedFlight = {...updatedFlight, ...discountedFlight}
+            return { ...discount, discountPrice, flight: updatedFlight };
         }));
       return { ...flight, discounts } as FlightDto;
     }));
